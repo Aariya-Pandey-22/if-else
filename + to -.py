35f3f9d94f7f36dc12cc275e4d4619f366782236
Aariya-Pandey-22 <119661488+Aariya-Pandey-22@users.noreply.github.com>
2022-12-02 14:37:08 +0530

    Add files via upload

---
+++ + to -.py
@@ -0,0 +1,16 @@
+# a=int(input("enter the number"))
+# if a>0:
+#     print(-a)
+# elif a<0:
+#     print(-(a))
+# else :
+#     print(0)
+
+
+a=int(input("enter the number"))
+if a>0:
+    print(-a)
+elif a<0:
+    print(-(a))
+else:
+    print(0)
